d2d3c3edde5ec377db6f1acc14f9face6be263e6
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-01-29 13:56:34 +0530

    the required changes has been made

--- sum.c
+++ sum.c
@@ -1,11 +1,15 @@
 # include<stdio.h>
 # include<conio.h>
-void main()
+ void main()
 {
     int a,b,sum,mul;
     printf("Enter the value of a and b :");
     scanf("%d %d",&a,&b);
     sum=a+b;
     mul=a*b;
-    printf("The sum and product of a and b is %d and %d ",sum, mul);
+    printf("The sum of a and b is :%d \n",sum);
+    printf("The product of a and b is :%d \n",mul);
+
+    getch();
+
 }
