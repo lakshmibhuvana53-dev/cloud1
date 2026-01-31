cc8637fbabd3eaf2ec1c6986d65e59c43fb7fcfa
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-01-31 19:43:41 +0530

    the 2nd file is created

---
+++ answer.c
@@ -0,0 +1,24 @@
+#include<string.h>
+#include<stdio.h>
+
+
+
+int main(void)
+{
+    
+    char answer[100];
+    printf("what is your name ? ");
+    scanf("%s", answer);
+    printf("Hello, %s\n", answer);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
