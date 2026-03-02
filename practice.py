85189c447d3c37ebcede9648e616204af829425c
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-03-02 19:13:04 +0530

    the practice is completed

---
+++ practice.py
@@ -0,0 +1,6 @@
+# programming for typeconversion and type casting
+user_name=input("What is ur name ? \n")
+length_of_name=len(user_name)
+print(type(user_name))
+print(type(length_of_name))
+print("Number of letters your Name holds is : " + str(length_of_name))
