399c3123a94068503b07e34044204424f7fa2a95
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-03-02 16:39:17 +0530

    the python file is created

---
+++ python.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city = input("What is the name of the city ?\n")
+pet = input("What is the name of your pet ?\n")
+print("Your band name could be: " + city +"" + pet)
