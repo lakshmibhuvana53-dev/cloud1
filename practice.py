177787648b6b328f85dc41c9a120c578a577b0a9
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-03-04 18:59:02 +0530

    the example pgrm is added

--- practice.py
+++ practice.py
@@ -4,3 +4,11 @@ length_of_name=len(user_name)
 print(type(user_name))
 print(type(length_of_name))
 print("Number of letters your Name holds is : " + str(length_of_name))
+
+# conditional operators and a example program
+print("Welcome to Roller Coaster!")
+height = int(input("What is your height in cm ? "))
+if height >= 120:
+    print("You can ride the roller coaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
