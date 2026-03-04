b582cf5c15c2bd54bc320304c0572997b5799e82
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-03-04 19:15:45 +0530

    the new example is created

--- practice.py
+++ practice.py
@@ -7,8 +7,18 @@ print("Number of letters your Name holds is : " + str(length_of_name))
 
 # conditional operators and a example program
 print("Welcome to Roller Coaster!")
-height = int(input("What is your height in cm ? "))
+height = int(input("What is your height in cm? \n"))
 if height >= 120:
     print("You can ride the roller coaster!")
 else:
     print("Sorry, you have to grow taller before you can ride.")
+     
+
+
+    #  to check if a number is even or odd
+
+    number_to_check = int(input("Enter the number you want to check : \n"))
+    if number_to_check % 2==0:
+        print("The number you entered is an even number")
+    else:
+        print("The number you entered is an odd number")
