80b2949c9ed5639ea81415cff03980ef9ae73f35
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-04-06 20:04:03 +0530

    the hangman file is in process

---
+++ hangman.py
@@ -0,0 +1,19 @@
+import random
+word_list = ["aaadvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(chosen_word)
+placeholder = " " 
+word_length = len(chosen_word)
+for _ in range(word_length):
+    placeholder += "_"
+print(placeholder)
+guess = input("Guess a letter: ").lower()
+print(guess)
+display = " "
+
+for letter in chosen_word:
+    if letter == guess:
+        display += letter
+    else:
+        display += "_"
+print(display)
